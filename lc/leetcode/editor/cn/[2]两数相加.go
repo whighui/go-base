95eb9493bf4e4cde8e29b113269c6d956a112ef1
whighui <whig1101@163.com>
2023-08-10 10:05:01 +0800

    add:lc-3

--- lc/leetcode/editor/cn/[2]两数相加.go
+++ lc/leetcode/editor/cn/[2]两数相加.go
@@ -1,55 +1,75 @@
-//给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。 
+//给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
 //
-// 请你将两个数相加，并以相同形式返回一个表示和的链表。 
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。
 //
-// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 //
-// 
 //
-// 示例 1： 
-// 
-// 
+//
+// 示例 1：
+//
+//
 //输入：l1 = [2,4,3], l2 = [5,6,4]
 //输出：[7,0,8]
 //解释：342 + 465 = 807.
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：l1 = [0], l2 = [0]
 //输出：[0]
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 //输出：[8,9,9,9,0,0,0,1]
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 每个链表中的节点数在范围 [1, 100] 内 
-// 0 <= Node.val <= 9 
-// 题目数据保证列表表示的数字不含前导零 
-// 
+//
+// 提示：
+//
+//
+// 每个链表中的节点数在范围 [1, 100] 内
+// 0 <= Node.val <= 9
+// 题目数据保证列表表示的数字不含前导零
+//
 //
 // Related Topics 递归 链表 数学 👍 9859 👎 0
 
+package doc
 
-//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+//
+// leetcode submit region begin(Prohibit modification and deletion)
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
+	carry := 0
+	result := &ListNode{} //这里边就是创建初始值嘞呢 并不是没创建
+	res := result
+	for l1 != nil || l2 != nil || carry != 0 {
+		val1, val2 := 0, 0
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+		sum := val1 + val2 + carry
+		result.Next = &ListNode{Val: sum % 10}
+		result = result.Next
+		carry = sum / 10
+	}
+	return res.Next
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
--- lc/leetcode/editor/cn/[3]无重复字符的最长子串.go
+++ lc/leetcode/editor/cn/[3]无重复字符的最长子串.go
@@ -1,46 +1,51 @@
-//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 //
-// 
+// 示例 1:
 //
-// 示例 1: 
+// 输入: s = "abcabcbb"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 //
-// 
-//输入: s = "abcabcbb"
-//输出: 3 
-//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-// 
+// 示例 2:
 //
-// 示例 2: 
+// 输入: s = "bbbbb"
+// 输出: 1
+// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
 //
-// 
-//输入: s = "bbbbb"
-//输出: 1
-//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-// 
+// 示例 3:
 //
-// 示例 3: 
+// 输入: s = "pwwkew"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 //
-// 
-//输入: s = "pwwkew"
-//输出: 3
-//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
-// 
+// 提示：
 //
-// 
-//
-// 提示： 
-//
-// 
-// 0 <= s.length <= 5 * 10⁴ 
-// s 由英文字母、数字、符号和空格组成 
-// 
+// 0 <= s.length <= 5 * 10⁴
+// s 由英文字母、数字、符号和空格组成
 //
 // Related Topics 哈希表 字符串 滑动窗口 👍 9453 👎 0
+package doc
 
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 func lengthOfLongestSubstring(s string) int {
 
+	//利用滑动窗口来实现 无重复字符的最长子串呢
+	if len(s) <= 1 {
+		return len(s)
+	}
+	left, right := 0, 0
+	m := make(map[uint8]int) //这里边就是使用空结构体就是可以了呢
+	for left <= right && right < len(s)-1 {
+		c := s[right]
+		m[c]++
+		for m[c] > 1 {
+			m[s[left]]--
+			left++
+		}
+		right++
+	}
+	return right - left
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
